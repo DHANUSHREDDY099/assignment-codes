8dbd1543a9308ec13be4892b8165173ce8490dff
KALASANI DHANUSH REDDY <123ec0010@iiitk.ac.in>
2023-10-07 19:13:21 +0530

    123EC0014'S ASSIGNMENT CODES

---
+++ 5.c
@@ -0,0 +1,18 @@
+// C Program to Calculate the Power of a Number
+
+#include <stdio.h>
+int main() {
+    int base, exp;
+    long double result = 1.0;
+    printf("Enter a base number: ");
+    scanf("%d", &base);
+    printf("Enter an exponent: ");
+    scanf("%d", &exp);
+
+    while (exp != 0) {
+        result *= base;
+        --exp;
+    }
+    printf("Answer = %.0Lf", result);
+    return 0;
+}

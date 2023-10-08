06638d88b88ee5a0ce0617241dde963a44692932
KALASANI DHANUSH REDDY <123ec0010@iiitk.ac.in>
2023-10-08 15:02:19 +0530

    123EC0047'S ASSIGNMENT CODES

---
+++ Q5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int age;
+
+  // take input from users
+  printf("Enter your age: ");
+  scanf("%d", &age);
+
+  // ternary operator to find if a person can vote or not
+  (age >= 18) ? printf("You can vote") : printf("You cannot vote");
+
+  return 0;
+}
